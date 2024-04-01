19ecda113d233d16a660cacd34cf809040ca4714
Alexia Aldea <alexia.elena.aldea@gmail.com>
2024-04-01 20:38:00 +0300

    added xcode project

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Alexia Aldea on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
